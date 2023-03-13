5f9562b08fbd732c88b1eca819b17eb6f8cdc039
rachith1234 <35251263+rachith1234@users.noreply.github.com>
2023-03-13 10:43:13 +0530

    make function
    make function for seller signUp Api call

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit } from '@angular/core';
+import { SellService } from '../services/sell.service';
 
 @Component({
   selector: 'app-seller-auth',
@@ -6,11 +7,14 @@ import { Component,OnInit } from '@angular/core';
   styleUrls: ['./seller-auth.component.css']
 })
 export class SellerAuthComponent implements OnInit {
-  constructor() {}
+  constructor(private sell:SellService) {}
 
-  ngOnInit(): void {
-    
-  }
+    ngOnInit(): void {}
+    signUp(data:Object):void{
+      console.warn(data)
+      this.sell.userSignUp();
+    }
+  
   onSubmit(data:object):void{
   console.warn(data);
 }
