0c26e08d3237f9b61e30c052945115e4c50777c9
rachith1234 <35251263+rachith1234@users.noreply.github.com>
2023-03-13 16:09:25 +0530

    changes

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { SellService } from '../services/sell.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-seller-auth',
@@ -7,12 +8,14 @@ import { SellService } from '../services/sell.service';
   styleUrls: ['./seller-auth.component.css']
 })
 export class SellerAuthComponent implements OnInit {
-  constructor(private sell:SellService) {}
+  constructor(private sell:SellService, private router:Router) {}
 
     ngOnInit(): void {}
     signUp(data:object):void{
         this.sell.userSignUp(data).subscribe((result)=>{
-        console.warn(result);
+        if(result){
+          this.router.navigate(['seller-home'])
+        }
       });
     }
   
