76d51a1ff8a31fbee4adb701cb268665eea2573f
rachith1234 <35251263+rachith1234@users.noreply.github.com>
2023-03-14 08:59:00 +0530

    create a interface

--- src/app/services/sell.service.ts
+++ src/app/services/sell.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { SignUp } from '../data-type';
 
 
 
@@ -10,7 +11,7 @@ export class SellService {
 
   constructor(private http:HttpClient) { }
 
-  userSignUp(data:any){
-   return this.http.post('http://localhost:3000/posts/',data)
+  userSignUp(data:SignUp){
+   return this.http.post('http://localhost:3000/posts/' ,data)
   }
 }
